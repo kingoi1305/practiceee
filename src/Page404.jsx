c8ab757b2e0fb95ad095e6dbe3e0351465bc5dcf
kin <89284325+kinkinkin1305@users.noreply.github.com>
2022-02-04 12:54:28 +0900

    404page

---
+++ src/Page404.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export const Page404 = () => {
+  return (
+    <div>
+      <h1>Not found Page</h1>
+      <Link to="/">Back Top</Link>
+    </div>
+  );
+};
